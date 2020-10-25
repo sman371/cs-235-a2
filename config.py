804b16e775e35db3e037ac1b6c7fca7a31fec4c2
sman371 <sman371@aucklanduni.ac.nz>
2020-10-25 22:44:26 +1300

    Secret key

--- config.py
+++ config.py
@@ -13,5 +13,17 @@ class Config:
     FLASK_ENV = environ.get('FLASK_ENV')
     SECRET_KEY = environ.get('SECRET_KEY')
 
+    # Flask variables
+    # ---------------
+    FLASK_APP = 'wsgi.py'
+    FLASK_ENV = 'development'  # 'development' or 'production'
+    SECRET_KEY = '+Fus{=8MSV99y2ahF:@gcjtB_&J7f?}g'  # Used to encrypt session data.
+    TESTING = False  # True or False.
+
+    # WTForm variables
+    # ----------------
+    WTF_CSRF_SECRET_KEY = '$=H}j62u&SyJCy,JGELHx&3$jr6`>T3Y'  # Needed by Flask WTForms to combat cross-site request forgery.
+
+
 
 
